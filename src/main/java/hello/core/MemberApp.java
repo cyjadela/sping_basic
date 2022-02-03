a0bec21dce586c0a64e1e546108a1318238c467d
cyjadela <adelacho@naver.com>
2022-02-03 19:12:22 +0900

    spring study 4주차

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+
 
         //테스트 해보기 -> 자바로 구현 --> test/member/MemberServiceTest로 대신할 수 있음
-        MemberService memberService=new MemberServiceImpl();
+        //MemberService memberService=new MemberServiceImpl(memberRepository);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -5,7 +5,12 @@ public class MemberServiceImpl implements MemberService{
 
     //구현 객체 선택해주기
     //좌변은 인터페이스에 의존하지만, 우변은 구현체에 의존하게 된다 (DIP위반)
-    private final MemberRepository memberRepository=new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     @Override
     public void join(Member member) {

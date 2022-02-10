34a2a6130500684376eb228f8498a2cb00326a19
cyjadela <adelacho@naver.com>
2022-02-10 22:00:06 +0900

    spring study 5주차

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,17 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 //MemberService에 대한 구현체
+@Component
 public class MemberServiceImpl implements MemberService{
 
     //구현 객체 선택해주기
     //좌변은 인터페이스에 의존하지만, 우변은 구현체에 의존하게 된다 (DIP위반)
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,9 +1,12 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
 //저장소
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     //저장소 만들기
